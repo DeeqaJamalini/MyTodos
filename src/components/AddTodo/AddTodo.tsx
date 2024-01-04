3138c25aec709600b8255ecd9c68ccea953f1ff0
DeeqaJamalini <deeqa.j11@gmail.com>
2024-01-04 10:43:30 +0000

    add-todo logic completed

--- src/components/AddTodo/AddTodo.tsx
+++ src/components/AddTodo/AddTodo.tsx
@@ -1,4 +1,41 @@
 import "./AddTodo.scss"
+import {useState} from 'react'
 
+type AddTodoProps = {
+    onAddTodo: (todo: string) => void;
+}
+
+
+
+ const AddTodo = ({onAddTodo} : AddTodoProps ) => {
+
+const [newTodo, setNewTodo] = useState<string>("");
+
+const handleAddTodo = () => {
+    if (newTodo.trim() !== "") {
+        onAddTodo(newTodo);
+        setNewTodo('');
+    }
+}
+
+    return (
+        <div>
+            <input
+            type= "text"
+            value= {newTodo}
+            onChange ={(e) => setNewTodo(e.target.value)}
+            
+            />
+            <button onClick={handleAddTodo}>+</button>
+            
+            
+
+        </div>
+
+    );
+
+};
+
+export default AddTodo;
 
 

174ecefd74c595a4645c049d117c8bcebf0a32a7
DeeqaJamalini <deeqa.j11@gmail.com>
2024-01-04 12:27:58 +0000

    delete-todo completed

--- src/components/DeleteTodo/DeleteTodo.tsx
+++ src/components/DeleteTodo/DeleteTodo.tsx
@@ -1,2 +1,18 @@
 import "./DeleteTodo.scss"
 
+
+
+type DeleteTodoProps = {
+    onDeleteTodo: () => void
+}
+
+
+const DeleteTodo = ({onDeleteTodo} : DeleteTodoProps) => {
+
+    return (
+        <button onClick={onDeleteTodo}>🗑️</button>
+    )
+
+}
+
+export default DeleteTodo

658fbd42f1ce6a3a4a8ae1f3c0c8889db3364ddc
DeeqaJamalini <deeqa.j11@gmail.com>
2024-01-04 11:25:18 +0000

    completed complete-todo

--- src/components/CompleteTodo/CompleteTodo.tsx
+++ src/components/CompleteTodo/CompleteTodo.tsx
@@ -1,1 +1,28 @@
 import "./CompleteTodo.scss"
+import {useState} from 'react'
+
+type CompleteTodoProps = {
+    onCompleteTodo: () => void;
+   
+}
+
+
+const CompleteTodo = ({onCompleteTodo}: CompleteTodoProps) => {
+
+    const[completed, setCompleted] = useState(false)
+
+    
+  const handleCompleteChange = () => {
+    setCompleted(!completed);
+    onCompleteTodo();
+  };
+
+
+    return (
+        <input type="checkbox" checked={completed} onChange={handleCompleteChange} />
+    )
+
+}
+
+
+export default CompleteTodo
